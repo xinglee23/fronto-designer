57a7e09761a205a767eecf5032fd5bae46657c22
xinglee23 <xinglee23@outlook.com>
2023-03-07 22:07:21 +0800

    feat: add defaultProps

---
+++ src/components/LText.vue
@@ -0,0 +1,42 @@
+<template>
+  <component
+    :is="tag"
+    :style="styleProps"
+    class="l-text-component"
+    @handleClick="handleClick"
+  >
+  </component>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+import useComponentCommon from '../hooks/useComponentCommon';
+import {
+  transformToComponentProps,
+  textDefaultProps,
+  textStylePropNames,
+} from '../defaultProps';
+const defaultProps = transformToComponentProps(textDefaultProps);
+
+export default defineComponent({
+  name: 'l-text',
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    ...defaultProps,
+  },
+  setup(props) {
+    const {styleProps, handleClick} = useComponentCommon(
+      props,
+      textStylePropNames
+    );
+
+    return {
+      styleProps,
+      handleClick,
+    };
+  },
+});
+</script>
+<style scoped></style>
